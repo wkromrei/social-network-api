0ffd7761898a0c7c63f50ae0518635f4a9b715f9
wkromrei <wkromrei@gmail.com>
2024-07-31 23:00:23 -0500

    Fixed routing issue.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,5 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-
 router.use((req, res) => res.send('Wrong route!'));
-
 module.exports = router;
